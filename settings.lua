aa3c4bbe1a6f2ac2a70c4a91a737dc466575238e
James-Fire <41241412+James-Fire@users.noreply.github.com>
2023-10-12 21:43:22 -0600

    Initial Commit

---
+++ settings.lua
@@ -0,0 +1,38 @@
+data:extend(
+{
+  {
+    type = "bool-setting",
+    name = "train-tiers",
+    setting_type = "startup",
+    default_value = false,
+	order = "a-1",
+  },
+  {
+    type = "bool-setting",
+    name = "weighty-trains",
+    setting_type = "startup",
+    default_value = false,
+	order = "b-1",
+  },
+  {
+    type = "bool-setting",
+    name = "powered-rails",
+    setting_type = "startup",
+    default_value = false,
+	order = "d-0",
+  },
+  {
+    type = "bool-setting",
+    name = "electric-trains",
+    setting_type = "startup",
+    default_value = false,
+	order = "d-1",
+  },
+  {
+    type = "bool-setting",
+    name = "electric-train-cars",
+    setting_type = "startup",
+    default_value = false,
+	order = "d-2",
+  },
+ })
