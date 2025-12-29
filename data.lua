946d38d56637a6876f3e311e7fefaaceeada2a52
James-Fire <41241412+James-Fire@users.noreply.github.com>
2025-12-29 12:32:40 -0700

    2.0.21

--- data.lua
+++ data.lua
@@ -16,33 +16,6 @@ data.raw["item-with-entity-data"]["locomotive"].icons = {
 		icon_mipmaps = 2,
 	},
 }
-
-if settings.startup["train-tiers"].value then
-	data.raw.locomotive["locomotive"].max_power = "500kW"
-	data.raw.locomotive["locomotive"].max_speed = 0.6
-	require("prototypes/train-tiers")
-end
-if settings.startup["train-car-tiers"].value then
-	require("prototypes/train-car-tiers")
-end
-if settings.startup["hybrid-trains"].value then
-	require("prototypes/hybrid-trains")
-end
-if settings.startup["electric-trains"].value then
-	require("prototypes/electric-trains")
-end
-if settings.startup["nuclear-trains"].value and settings.startup["hybrid-trains"].value then
-	require("prototypes/nuclear-hybrid-trains")
-end
-if settings.startup["nuclear-trains"].value then
-	require("prototypes/nuclear-trains")
-end
-if settings.startup["electric-train-cars"].value then
-	require("prototypes/electric-train-cars")
-end
-if settings.startup["powered-rails"].value then
-	require("prototypes/powered-rails")
-end
 --[[if settings.startup["variable-weighty-trains"].value then
 	require("prototypes/train-weight")
 end]]
